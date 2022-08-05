2ccaef477b1e7971b04ddf37fbe7c6d0268d53d8
Peter Chavez <107886587+Peter-N-Chavez@users.noreply.github.com>
2022-08-05 16:59:39 -0500

    Updates from classification exercises.

---
+++ acquire.py
@@ -0,0 +1,26 @@
+import env
+import os
+
+def get_db_url(hostname, username, password, database):
+    url = f'mysql+pymysql://{username}:{password}@{hostname}/{database}'
+    return url
+
+
+def get_titanic_data():
+    filename = "titanic.csv"
+    
+    # if file is available locally, read it
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    
+    # if file not available locally, acquire data from SQL database
+    # and write it as csv locally for future use
+    else:
+        # read the SQL query into a dataframe
+        df = new_titanic_data()
+        
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df  
