bd0885f1229400685cd65fa26847a3535871bb91
Peter Chavez <107886587+Peter-N-Chavez@users.noreply.github.com>
2022-08-09 15:10:37 -0500

    Updates for tidy_data exercises.

--- acquire.py
+++ acquire.py
@@ -8,6 +8,24 @@ import seaborn as sns
 from pydataset import data
 import os
 
+
+# def ds_imports():
+
+#     import numpy as np
+#     import scipy.stats as stats
+#     import pandas as pd
+#     import matplotlib.pyplot as plt
+#     import matplotlib.image as mpimg
+#     import seaborn as sns
+#     from pydataset import data
+#     from sklearn.model_selection import train_test_split
+#     from sklearn.impute import SimpleImputer
+#     from acquire import *
+#     from prepare import *
+
+
+
+
 def get_db_url(hostname, username, password, database):
     url = f'mysql+pymysql://{username}:{password}@{hostname}/{database}'
     return url
@@ -17,6 +35,7 @@ def new_tit_df():
     url_tit = get_db_url(env.hostname, env.username, env.password, "titanic_db")
     query = pd.read_sql('''SELECT * \
                          FROM passengers; ''', url_tit)
+    return query
 
 def get_titanic_data():
     filename = "titanic.csv"
@@ -79,3 +98,81 @@ def get_telco_data():
     df = pd.read_sql(sql_query, get_db_url(env.hostname, env.username,env.password, "telco_churn"))
     
     return df
+
+def get_coffee_data():
+    filename = "coffee_levels.csv"
+    
+    # if file is available locally, read it
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    
+    # if file not available locally, acquire data from SQL database
+    # and write it as csv locally for future use
+    else:
+        # read the SQL query into a dataframe
+        df = new_coffee_df()
+        
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+
+def new_coffee_df():
+
+    url_tidy_data = get_db_url(env.hostname, env.username, env.password, "tidy_data")
+    query = pd.read_sql('''SELECT * \
+                         FROM coffee_levels; ''', url_tidy_data)
+    return query
+
+def get_attendance_data():
+    filename = "attendance.csv"
+    
+    # if file is available locally, read it
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    
+    # if file not available locally, acquire data from SQL database
+    # and write it as csv locally for future use
+    else:
+        # read the SQL query into a dataframe
+        df = new_coffee_df()
+        
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+
+def new_attendance_df():
+
+    url_tidy_data = get_db_url(env.hostname, env.username, env.password, "tidy_data")
+    query = pd.read_sql('''SELECT * \
+                         FROM attendance; ''', url_tidy_data)
+    return query
+
+def get_cake_data():
+    filename = "cake_recipes.csv"
+    
+    # if file is available locally, read it
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    
+    # if file not available locally, acquire data from SQL database
+    # and write it as csv locally for future use
+    else:
+        # read the SQL query into a dataframe
+        df = new_coffee_df()
+        
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+
+def new_cake_df():
+
+    url_tidy_data = get_db_url(env.hostname, env.username, env.password, "tidy_data")
+    query = pd.read_sql('''SELECT * \
+                         FROM cake_recipes; ''', url_tidy_data)
+    return query
--- prepare.py
+++ prepare.py
@@ -7,32 +7,68 @@ import seaborn as sns
 import os
 import acquire
 
-def prep_iris():
+def prep_iris(df):
 
-    df = acquire.get_iris_data()
     df = df.drop(columns=["species_id", "measurement_id"])
     df = df.rename(columns={"species_name": "species"})
     dummy_df = pd.get_dummies(df[["species"]], drop_first=True)
     df = pd.concat([df, dummy_df], axis=1)
     return df
 
-def prep_telco():
-    df = acquire.get_telco_data()
-    df = df.drop(columns = ["contract_type", "payment_type",
-                       "internet_service_type"])
-    dummy_df = pd.get_dummies(df[["gender"]], drop_first=True)
-    df = pd.concat([df, dummy_df], axis=1)
-    return df
+
+
+def prep_tit(titanic):
+    titanic = titanic.drop(columns=['embarked','class', 'age','deck'])
+    dummy_df = pd.get_dummies(data=titanic[['sex','embark_town']], drop_first=True)
+    titanic = pd.concat([titanic, dummy_df], axis=1)
+    
+    return titanic
+
+
+
+def prep_telco(telco):
+    telco = telco.drop(columns=['internet_service_type_id', 'contract_type_id', 'payment_type_id'])
+
+    telco['gender_encoded'] = telco.gender.map({'Female': 1, 'Male': 0})
+    telco['partner_encoded'] = telco.partner.map({'Yes': 1, 'No': 0})
+    telco['dependents_encoded'] = telco.dependents.map({'Yes': 1, 'No': 0})
+    telco['phone_service_encoded'] = telco.phone_service.map({'Yes': 1, 'No': 0})
+    telco['paperless_billing_encoded'] = telco.paperless_billing.map({'Yes': 1, 'No': 0})
+    telco['churn_encoded'] = telco.churn.map({'Yes': 1, 'No': 0})
+    
+    dummy_df = pd.get_dummies(telco[['multiple_lines', \
+                              'online_security', \
+                              'online_backup', \
+                              'device_protection', \
+                              'tech_support', \
+                              'streaming_tv', \
+                              'streaming_movies', \
+                              'contract_type', \
+                              'internet_service_type', \
+                              'payment_type'
+                            ]],
+                              drop_first=True)
+    telco = pd.concat( [telco, dummy_df], axis=1 )
+    
+    return telco
     
 # Split your data
 # #
 # 
 # 
-#  Write a function to split your data into train, test and validate datasets. Add
-# 
-# 
-#  this function to prepare.py.
-# #
+#  Write a function to split your data into train, test and validate datasets. Addthis function to prepare.py.
+
+
+
+def prep_split(df, target):
+    
+    train, test = train_test_split(df, test_size=.2, random_state=269, stratify=df[target])
+    train, validate = train_test_split(train, test_size=.25, random_state=269, stratify=train[target])
+    
+    return train, validate, test
+
+
+
 # 
 # 
 #  Run the function in your notebook on the Iris dataset, returning 3 datasets,
@@ -53,3 +89,4 @@ def prep_telco():
 # 
 # 
 #  validate_telco and test_telco.
+
